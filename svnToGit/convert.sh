9a6f86fc3aef30d3ec8badaa06fe18a784fb1603
Lynn Garren <garren@fnal.gov>
2014-01-16 16:09:38 -0600

    convert svn to v0_00_01 (alpha)

---
+++ svnToGit/convert.sh
@@ -0,0 +1,405 @@
+#!/bin/bash
+
+# convert svn to get
+# select only desired packages for each new git repository
+
+source /products/setup
+setup git
+
+working_dir=/home/garren/scratch/larsoft
+author_list=/home/garren/larsoft/notes/larsoft.git.author.list
+larsoft_svn=http://cdcvs.fnal.gov/subversion/larsoftsvn
+lardir=${working_dir}/gitlar
+larsoft_dir_list="larcore larexamples lardata larevt larsim larreco larana lareventdisplay larsoft"
+
+if [ -d ${lardir} ]
+then
+  echo "ERROR: ${lardir} already exists - we need to create an empty directory"
+  exit 1
+fi
+
+
+for dir in ${larsoft_dir_list}
+do
+  fulldir=${lardir}/${dir}
+  echo "making ${fulldir}"
+  mkdir -p ${fulldir} || ( echo "failed to create ${fulldir}"; exit 1; )
+done
+
+cd ${lardir}/larcore
+echo "_____________________________________________________________________"
+echo "make larcore"
+svn2git ${larsoft_svn} --authors ${author_list} --verbose \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude Utilities \
+--exclude VertexFinder \
+ >& ${lardir}/log.larcore.convert &
+
+cd ${lardir}/lardata
+echo "_____________________________________________________________________"
+echo "make lardata"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude 'OpticalDetector\b' \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RecoAlg \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude VertexFinder \
+ >& ${lardir}/log.lardata.convert &
+
+cd ${lardir}/larevt
+echo "_____________________________________________________________________"
+echo "make larevt"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude Utilities \
+--exclude VertexFinder \
+ >& ${lardir}/log.larevt.convert &
+
+cd ${lardir}/larsim
+echo "_____________________________________________________________________"
+echo "make larsim"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude Filters \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude Utilities \
+--exclude VertexFinder \
+ >& ${lardir}/log.larsim.convert &
+
+cd ${lardir}/larreco
+echo "_____________________________________________________________________"
+echo "make larreco"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Geometry \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TriggerAlgo \
+--exclude Utilities \
+ >& ${lardir}/log.larreco.convert &
+
+cd ${lardir}/larana
+echo "_____________________________________________________________________"
+echo "make larana"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetectorData \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude Utilities \
+--exclude VertexFinder \
+ >& ${lardir}/log.larana.convert &
+
+cd ${lardir}/lareventdisplay
+echo "_____________________________________________________________________"
+echo "make lareventdisplay"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude Utilities \
+--exclude VertexFinder \
+ >& ${lardir}/log.lareventdisplay.convert &
+
+cd ${lardir}/larexamples
+echo "_____________________________________________________________________"
+echo "make larexamples"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude Utilities \
+--exclude VertexFinder \
+ >& ${lardir}/log.larexamples.convert &
+
+cd ${lardir}/larsoft
+echo "_____________________________________________________________________"
+echo "make larsoft"
+svn2git ${larsoft_svn} --authors ${author_list} \
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude Utilities \
+--exclude VertexFinder \
+ >& ${lardir}/log.larsoft.convert &
+
+
+exit 0
+
+# list of svn packages
+
+--exclude AnalysisAlg \
+--exclude AnalysisBase \
+--exclude AnalysisExample \
+--exclude CalData \
+--exclude Calorimetry \
+--exclude ClusterFinder \
+--exclude DetSim \
+--exclude EventDisplay \
+--exclude EventFinder \
+--exclude EventGenerator \
+--exclude Filters \
+--exclude Geometry \
+--exclude Genfit \
+--exclude HitFinder \
+--exclude LArG4 \
+--exclude MCCheater \
+--exclude Monitoring \
+--exclude OpticalDetector \
+--exclude OpticalDetectorData \
+--exclude ParticleIdentification \
+--exclude PhotonPropagation \
+--exclude RawData \
+--exclude RecoAlg \
+--exclude RecoBase \
+--exclude RecoObjects \
+--exclude setup \
+--exclude ShowerFinder \
+--exclude SimpleTypesAndConstants \
+--exclude Simulation \
+--exclude SRT_LAR \
+--exclude SummaryData \
+--exclude TrackFinder \
+--exclude TriggerAlgo \
+--exclude Utilities \
+--exclude VertexFinder \
+
+
