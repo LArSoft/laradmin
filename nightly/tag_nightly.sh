ddd7e4e7a280eaf16c64cf3e32e535a4a298c963
Lynn Garren <garren@fnal.gov>
2014-01-30 12:42:17 -0600

    use cetbuildtools v3_07_06

--- nightly/tag_nightly.sh
+++ nightly/tag_nightly.sh
@@ -18,14 +18,14 @@ update_tag()
     exit 1
   fi
   # we need the new product-config.cmake.in
-  cp -p /grid/fermiapp/products/larsoft/cetbuildtools/v3_07_05/templates/product-config.cmake.in.template ups/product-config.cmake.in || exit 1;
+  cp -p /grid/fermiapp/products/larsoft/cetbuildtools/v3_07_06/templates/product-config.cmake.in.template ups/product-config.cmake.in || exit 1;
   # need to use a fixit script here
   fixfile=${larpkg}.fix.sh
   rm -f ${fixfile}
   echo "#!/bin/bash" > ${fixfile}
   echo "set -x" >> ${fixfile}
   echo "mv ups/product_deps ups/product_deps.bak || exit 1;" >> ${fixfile}
-  echo "cat ups/product_deps.bak | sed -e 's%$version%nightly%g' | sed -e 's%v3_07_04%v3_07_05%' > ups/product_deps || exit 1;" >> ${fixfile}
+  echo "cat ups/product_deps.bak | sed -e 's%$version%nightly%g' | sed -e 's%v3_07_04%v3_07_06%' > ups/product_deps || exit 1;" >> ${fixfile}
   echo "set +x" >> ${fixfile}
   echo "exit 0" >> ${fixfile}
   chmod +x ${fixfile} || exit 1;
