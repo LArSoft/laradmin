1e952b986abbc44c5ac37b978de208766b7fed35
Lynn Garren <garren@fnal.gov>
2014-01-31 12:04:05 -0600

    fix the product path for lbne and uboone

--- nightly/config_nightly.sh
+++ nightly/config_nightly.sh
@@ -27,7 +27,7 @@ case "$PROJECT" in
     ;;
   uboone|lbne)
     PKGLIST="${PROJECT}code"
-    PROJ_PRODUCTS="/grid/fermiapp/$PROJECT/software/products/${PROJECT}code"
+    PROJ_PRODUCTS="/grid/fermiapp/$PROJECT/software/products"
     SETUPS="/grid/fermiapp/$PROJECT/software/setup_${PROJECT}.sh"
     if [ $PROJECT = lbne ]
     then
