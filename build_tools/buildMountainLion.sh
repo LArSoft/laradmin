8eae6b94127707496a18835a455528198d7fad2b
Lynn Garren <garren@fnal.gov>
2014-07-03 10:29:15 -0500

    try a copy

--- build_tools/buildMountainLion.sh
+++ build_tools/buildMountainLion.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-pwd
-ls
-
 work_dir=${1}
 version=${2}
 
+echo "working directory: ${work_dir}"
 ls ${work_dir}
 
 echo "version is ${version}"
+
+cd ${work_dir}
+curl --fail --silent --location --insecure -O http://oink.fnal.gov/distro/larsoft/buildLAr-${version}
+curl --fail --silent --location --insecure -O http://oink.fnal.gov/distro/larsoft/pullProductsLAr-${version}
+ls
+
 
