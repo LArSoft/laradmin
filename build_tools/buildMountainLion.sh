5200be1e9d9ca1d2c29403c1a5f08200c4faa24f
Lynn Garren <garren@fnal.gov>
2014-07-03 11:25:45 -0500

    try a build now

--- build_tools/buildMountainLion.sh
+++ build_tools/buildMountainLion.sh
@@ -6,7 +6,11 @@ version=${2}
 echo "working directory: ${work_dir}"
 echo "version is ${version}"
 
-cd ${work_dir}
+if [ ! -d ${work_dir}/build ]
+then
+ mkdir -p ${work_dir}/build
+fi
+cd ${work_dir}/build || exit 1
 curl --fail --silent --location --insecure -O http://oink.fnal.gov/distro/larsoft/buildLAr-${version}
 curl --fail --silent --location --insecure -O http://oink.fnal.gov/distro/larsoft/pullProductsLAr-${version}
 ls
@@ -15,5 +19,10 @@ chmod +x buildLAr-${version}
 ./pullProductsLAr-${version} ${work_dir} source larsoft || exit 1
 ./pullProductsLAr-${version} ${work_dir} d12 nu e5 debug || exit 1
 ./pullProductsLAr-${version} ${work_dir} d12 nu e5 prof || exit 1
+echo
+echo "begin build"
+echo
+./buildLAr-${version} -t ${work_dir} debug larsoft || exit 1
+./buildLAr-${version} -t ${work_dir} prof larsoft || exit 1
 
 exit 0
