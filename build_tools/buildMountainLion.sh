f871d17955b6cd9747590e0a0241900415a07c6c
Lynn Garren <garren@fnal.gov>
2014-07-03 10:46:10 -0500

    populate the directory

--- build_tools/buildMountainLion.sh
+++ build_tools/buildMountainLion.sh
@@ -4,13 +4,16 @@ work_dir=${1}
 version=${2}
 
 echo "working directory: ${work_dir}"
-ls ${work_dir}
-
 echo "version is ${version}"
 
 cd ${work_dir}
 curl --fail --silent --location --insecure -O http://oink.fnal.gov/distro/larsoft/buildLAr-${version}
 curl --fail --silent --location --insecure -O http://oink.fnal.gov/distro/larsoft/pullProductsLAr-${version}
 ls
-
+chmod +x pullProductsLAr-${version}
+chmod +x buildLAr-${version}
+./pullProductsLAr-${version} ${work_dir} source larsoft
+./pullProductsLAr-${version} ${work_dir} d12 larsoft e5 debug
+./pullProductsLAr-${version} ${work_dir} d12 larsoft e5 prof
+ls
 
