2ce5ecc387a586161dd7fa9bc43dd27a8bcabdb8
Lynn Garren <garren@fnal.gov>
2014-08-06 12:59:43 -0500

    build Mavericks in a vm

---
+++ build_tools/buildMavericks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# run by hand on a vm on LG's laptop
+
+work_dir=${1}
+version=${2}
+
+echo "working directory: ${work_dir}"
+echo "version is ${version}"
+
+working_dir=${work_dir}
+if [ ! -d ${work_dir} ]
+then
+ mkdir -p ${work_dir}
+fi
+cd ${work_dir} || exit 1
+curl --fail --silent --location --insecure -O http://oink.fnal.gov/distro/larsoft/pullProductsLAr-${version} || exit 1
+chmod +x pullProductsLAr-${version} || exit 1
+./pullProductsLAr-${version} ${working_dir} source larsoft || exit 1
+# pulling binaries is allowed to fail
+./pullProductsLAr-${version} ${working_dir} d13 nu e5 debug 
+./pullProductsLAr-${version} ${working_dir} d13 nu e5 prof 
+echo
+echo "begin build"
+echo
+./buildLAr-${version} -t ${working_dir} debug larsoft || exit 1
+./buildLAr-${version} -t ${working_dir} prof larsoft || exit 1
+
+exit 0
