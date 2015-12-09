dcdc908a3605482743a19c458bba3022cbf008fa
Lynn Garren <garren@fnal.gov>
2015-12-09 10:14:31 -0600

    generate hash for use in perl script

---
+++ refactor/generate_hash.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -z "${MRB_SOURCE}" ]
+then
+    echo 'ERROR: MRB_SOURCE is not defined'
+    exit 1
+fi
+if [ ! -r ${MRB_SOURCE}/CMakeLists.txt ]; then
+    echo "${MRB_SOURCE}/CMakeLists.txt not found"
+    exit 1
+fi
+
+larlist="larcore lardata larevt larsim lareventdisplay larexamples larreco larpandora larana"
+
+hash_file=${PWD}/header_hash.pl
+echo > ${hash_file}
+echo "dirs = (" >>  ${hash_file}
+
+for REP in $larlist
+do
+   echo
+   echo "begin ${REP} ${version}"
+   cd ${MRB_SOURCE}/${REP}/${REP} || exit 1
+   reflist=""
+   list=`ls -1`
+   for subdir in $list
+   do
+      if [ -d $subdir ]
+      then
+	  reflist="$reflist $subdir"
+      fi
+   done
+   echo "       ${REP} => [qw( ${reflist} )]"
+   echo "       ${REP} => [qw( ${reflist} )]" >>  ${hash_file}
+done
+echo "     )" >>  ${hash_file}
+
+exit 0
+
