8812a6e0f3f3b1e85b410a4d8630348709f65e37
Lynn Garren <garren@fnal.gov>
2022-01-18 12:25:01 -0600

    Explicit_code_changes_since_v06_18_00 is a release page

--- gitwiki/doit.sh
+++ gitwiki/doit.sh
@@ -85,6 +85,7 @@ move_files() {
   mv LArSoft_release_list.md releases/ || { echo "ERROR: mv LArSoft_release_list.md failed"; exit 1; }
   mv Initial_Releases.md releases/ || { echo "ERROR: mv Initial_Releases.md failed"; exit 1; }
   mv FutureChanges.md releases/ || { echo "ERROR: mv FutureChanges.md failed"; exit 1; }
+  mv Explicit_code_changes_since_v06_18_00.md releases/ || { echo "ERROR: mv Explicit_code_changes_since_v06_18_00.md failed"; exit 1; }
   mv Breaking_Changes*.md releases/ || { echo "ERROR: mv Breaking_Changes*.md failed"; exit 1; }
   sed -i -e 's%(LArSoft_release_list)%(releases/LArSoft_release_list)%g' *.md
   sed -i -e 's%(FutureChanges)%(releases/FutureChanges)%g' *.md
