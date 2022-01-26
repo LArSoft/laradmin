38a97ecaada448ce506d01d56b24152f4ebbbe9d
Lynn Garren <garren@fnal.gov>
2022-01-25 23:31:31 -0600

    remove debug output

--- gitwiki/convert_source.pl
+++ gitwiki/convert_source.pl
@@ -58,22 +58,22 @@ sub process_textile {
     $line =~ s/\\_/_/g;
     $newline = $line;
     if( $line =~ m/\Q\[\[/ ) {
-      print "fix $line\n";
+      #print "fix $line\n";
       $p1 = index($line, '\[\[');
       $p2 = index($line, '\|', $p1);
       $p3 = index($line, '\]\]', $p1);
-      print "found $p1 - $p2 - $p3\n";
+      #print "found $p1 - $p2 - $p3\n";
       if ( $p2 > $p1 ) {
         $nl1 = substr $line, 0, $p1;
         $nl2 = substr $line, $p1+4, $p2-$p1-4;
         $nl3 = substr $line, $p2+2, $p3-$p2-2;
         $nl4 = substr $line, $p3+4;
         $pc = index($nl2, ':');
-        print "found $pc in $nl2\n";
+        #print "found $pc in $nl2\n";
         if ( $pc > 0 ) {
           my $nl21 = substr $nl2, 0, $pc;
           my $nl22 = substr $nl2, $pc+1;
-          print "nl2 splits into %$nl21%$nl22%\n";
+          #print "nl2 splits into %$nl21%$nl22%\n";
           $nl2 = "https://cdcvs.fnal.gov/redmine/projects/".$nl21."/wiki/".$nl22
         }
         #print " $nl1\n";
@@ -88,11 +88,11 @@ sub process_textile {
         $nl2 = $nl3;
         $nl2 =~ s% %_%g;
         $pc = index($nl2, ':');
-        print "found $pc in $nl2\n";
+        #print "found $pc in $nl2\n";
         if ( $pc > 0 ) {
           my $nl21 = substr $nl2, 0, $pc;
           my $nl22 = substr $nl2, $pc+1;
-          print "nl2 splits into %$nl21%$nl22%\n";
+          #print "nl2 splits into %$nl21%$nl22%\n";
           $nl2 = "https://cdcvs.fnal.gov/redmine/projects/".$nl21."/wiki/".$nl22
         }
         #print " $nl1\n";
